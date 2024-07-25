71c7a00fe17cc7b6b5afa24d40b1a779dbec735e
JBEDU29 <5514cyj@gmail.com>
2024-07-25 20:18:30 +0900

    신규회원 가입만 가능

---
+++ memberDB.py
@@ -0,0 +1,34 @@
+import pymysql
+
+conn = pymysql.connect(host='localhost',user='root',password='12345', db='memberdb')
+
+print("*********************회원관리 프로그램 ********************")
+print("(1)회원가입")
+print("(2)회원정보수정")
+print("(3)회원탈퇴")
+print("(4)회원전체목록조회")
+print("(5)프로그램 종료")
+print("*********************************************************")
+
+userNum=input("위 메뉴 중 한 가지를 선택하세요(1~5)  :")
+
+if userNum == "1" :
+    print("가입하시려면 회원정보를 입력하세요")
+    memberid=input("* 회원아이디를 입력하세요 :")
+    membername=input("* 회원이름을 입력하세요 :")
+    memberemail=input("* 이메일을 입력하세요 :")
+    memberage=input("* 나이를 입력하세요 :")
+
+    sql=F"INSERT INTO membertbl(memberid,membername,memberemail,memberage) VALUES('{memberid}','{membername}','{memberemail}','{memberage}')"
+
+    cur=conn.cursor()  #cursor 생성
+    success = cur.execute(sql)  #sql문 실행 -> 반환되는 값이 1이면 '성공'
+
+    if success == 1:
+        print("축하합니다! 회원가입 성공하셨습니다.")
+    else:
+        print("회원가입 실패입니다.")
+
+    cur.close()
+    conn.commit()
+    conn.close()
